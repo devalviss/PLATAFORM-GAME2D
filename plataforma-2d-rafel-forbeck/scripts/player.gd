61a0cf533f47cc77aac1fe7ba3bc82c9908cbab9
alves <alvesalph@gmail.com>
2026-06-07 23:51:47 -0300

    Adicionando 3 fases iniciais

---
+++ plataforma-2d-rafel-forbeck/scripts/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 90.0
+const JUMP_VELOCITY = -200.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if is_on_floor():
+		
+		if direction > 0:
+			anim.flip_h = false
+			anim.play("Walk")
+		elif direction < 0:
+			anim.flip_h = true
+			anim.play("Walk")
+		else: 
+			anim.play("Idle")
+	else:
+		anim.play("Jump")
+
+	move_and_slide()
